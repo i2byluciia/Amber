7d79d3509197711ae3e280dadeff7d2f2419cb25
i2byluciia <luciiacampos.27@gmail.com>
2026-09-01 13:11:57 +0200

    create amber core

---
+++ core.py
@@ -0,0 +1,7 @@
+def process_input(user_input):
+    response = generate_response(user_input)
+    return response
+
+
+def generate_response(user_input):
+    return f"You said '{user_input}'"
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hola, soy Amber")
+from core import process_input
+
+def main():
+    print("Amber is starting...")
+
+    while True:
+        user_input = input("You: ")
+        
+        if user_input.lower() in ["exit"]:
+            print("Amber: Goodbye! ")
+            break
+
+        response = process_input(user_input)
+        print(f"Amber: {response}")
+
+if __name__ == "__main__":
+    main()
